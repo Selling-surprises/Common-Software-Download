2dd0d46d17828bd966fcd92f382259f46681f4a9
Selling-surprises <163849232+Selling-surprises@users.noreply.github.com>
2026-09-05 20:45:40 +0800

    Add files via upload

---
+++ navigation-data.js
@@ -0,0 +1,332 @@
+// 本文件由「常用软件导航制作器」导出，请与 navigation.html 放在同一目录
+// 可直接导入回制作器：数据导入 → 选择本文件
+window.NAV_DATA = {
+  "groups": [
+    {
+      "id": "mtk4y2ts03i8",
+      "name": "常用软件",
+      "icon": "pin",
+      "color": "",
+      "links": [
+        {
+          "id": "mtk50noov82k",
+          "name": "腾讯QQ",
+          "url": "https://im.qq.com/index/#/",
+          "note": "QQ（原名：OICQ，又名腾讯QQ），是深圳市腾讯计算机系统有限公司推出的一款基于互联网的即时通信软件。",
+          "icon": "https://static-res.qq.com/static-res/imqq/qq-logo.png"
+        },
+        {
+          "id": "mtk51jh4z583",
+          "name": "微信",
+          "url": "https://weixin.qq.com/",
+          "note": "微信（WeChat），是腾讯公司于2011年1月21日推出的一个为智能终端提供即时通讯服务的免费应用程序。",
+          "icon": "https://res.wx.qq.com/a/wx_fed/assets/res/NTI4MWU5.ico"
+        },
+        {
+          "id": "mtk5pnqgraa0",
+          "name": "企业微信",
+          "url": "https://work.weixin.qq.com/",
+          "note": "企业微信是腾讯微信团队为企业打造的专业办公管理工具，与微信一致的沟通体验。",
+          "icon": "https://work.weixin.qq.com/favicon.ico"
+        },
+        {
+          "id": "mtk56500z6li",
+          "name": "WPS Office",
+          "url": "https://www.mefcl.com/wps-office-2023.html",
+          "note": "WPS Office是由北京金山办公软件股份有限公司自主研发的办公软件套装。",
+          "icon": "https://ee.wpscdn.cn/wpscn/favicon.ico"
+        },
+        {
+          "id": "mtk5s8cwgscn",
+          "name": "钉钉",
+          "url": "https://www.dingtalk.com/",
+          "note": "钉钉是阿里巴巴集团打造的企业级智能移动办公平台，是数字经济时代的企业组织协同办公和应用开发平台。",
+          "icon": "https://pc3.gtimg.com/softmgr/logo/48/26980_48_1775182015.png"
+        },
+        {
+          "id": "mtk58xmckvch",
+          "name": "HiBit Uninstaller",
+          "url": "https://www.hibitsoft.ir/Uninstaller.html",
+          "note": "HiBit Uninstaller可以彻底卸载那些顽固的软件、Windows 应用程序以及浏览器扩展程序。",
+          "icon": "https://www.hibitsoft.ir/images/Uninstaller-icon.png"
+        },
+        {
+          "id": "mto9toqw4c5t",
+          "name": "图拉丁吧工具箱",
+          "url": "https://www.tbtool.cn/",
+          "note": "图吧工具箱，是开源、免费、绿色、纯净的硬件检测工具合集，专为所有计算机硬件极客、DIY爱好者、各路大神及小白制作。",
+          "icon": "https://www.tbtool.cn/favicon.ico"
+        }
+      ]
+    },
+    {
+      "id": "mtk1moxjjsw3",
+      "name": "浏览器",
+      "icon": "browser",
+      "color": "",
+      "links": [
+        {
+          "id": "mtk1oogfdsze",
+          "name": "360极速浏览器",
+          "url": "https://browser.360.cn/ee/",
+          "note": "全新Chrome内核浏览器，性能更强，速度更快。",
+          "icon": "https://browser.360.cn/ee/favicon.ico"
+        },
+        {
+          "id": "mtk1qt12eyig",
+          "name": "百分浏览器",
+          "url": "https://www.centbrowser.cn/history.html",
+          "note": "追求速度、简约和安全的网终刘览器！",
+          "icon": "https://www.centbrowser.cn/favicon.ico"
+        },
+        {
+          "id": "mtk1tlnn2v94",
+          "name": "Supermium",
+          "url": "https://win32subsystem.live/supermium/",
+          "note": "一款为数不多，还支持XP系统的浏览器。",
+          "icon": "https://win32subsystem.live/supermium/favicon.ico"
+        },
+        {
+          "id": "mtk1xfly981g",
+          "name": "Iceweasel",
+          "url": "https://sourceforge.net/projects/libportable/files/Iceweasel/Stable/",
+          "note": "Iceweasel（中文常称“冰鼬”）是一款基于 Mozilla Firefox 代码重新编译的开源浏览器",
+          "icon": "https://a.fsdn.com/allura/p/libportable/icon?1764055982"
+        },
+        {
+          "id": "mtk20a3prspg",
+          "name": "Microsoft Edge",
+          "url": "https://www.microsoftedgeinsider.com/zh-cn/download",
+          "note": "Microsoft Edge是微软基于Chromium开源项目及其他开源软件开发的网页浏览器，选择Beta版更好用！",
+          "icon": "https://explore.microsoft.com/apple-touch-icon.png"
+        },
+        {
+          "id": "mtk2bmtpksfm",
+          "name": "Google Chrome",
+          "url": "https://www.google.cn/chrome/",
+          "note": "Google Chrome是由谷歌公司开发的网页浏览器。",
+          "icon": "https://www.google.cn/chrome/static/images/favicons/android-icon-192x192.png"
+        },
+        {
+          "id": "mtoa8aq81bch",
+          "name": "Turbo Browser",
+          "url": "https://tbrowser.cn/zh",
+          "note": "一款纯粹的高效浏览器：启动快、占用低，Chrome / Edge 一键迁入，开箱即用。",
+          "icon": "https://tbrowser.cn/favicon.ico"
+        },
+        {
+          "id": "mtobph0mclso",
+          "name": "123浏览器",
+          "url": "https://123llq.com/",
+          "note": "‌123浏览器确实是一款以简约、安全为目标的 Chrome 内核增强版浏览器‌，并且在办公和娱乐上网体验上做了不少增强。‌",
+          "icon": "https://cdn-static.123llq.com/res/favicon.ico"
+        }
+      ]
+    },
+    {
+      "id": "mtjzp7bvaovr",
+      "name": "图片查看器",
+      "icon": "image",
+      "color": "",
+      "links": [
+        {
+          "id": "mtjzu82jzese",
+          "name": "果核看图",
+          "url": "https://pic.ghxi.com/",
+          "note": "可能是世界上最小的全格式看图软件。提供完整的图片解码能力，支持 HEIC、AVIF、WebP、RAW、PSD 等多种格式。",
+          "icon": "https://pic.ghxi.com/assets/images/logo.webp"
+        },
+        {
+          "id": "mtk0mwllbdy6",
+          "name": "PhoXoSee",
+          "url": "https://phoxo.com/",
+          "note": "没有广告，没有冗余内容，只有极致的速度体验，完全免费且采用开源技术。网页虽然是英文，但软件内置中文。",
+          "icon": "https://phoxo.com/images/logo.svg"
+        },
+        {
+          "id": "mtk0q2mxid7j",
+          "name": "灵猫看图",
+          "url": "https://lmkt.tianruo.xyz/",
+          "note": "简单清爽的看图体验。",
+          "icon": "https://lmkt.tianruo.xyz/Appicon.ico"
+        },
+        {
+          "id": "mtleq4ac8uer",
+          "name": "XnViewMP",
+          "url": "https://newsgroup.xnview.com/viewtopic.php?t=50191",
+          "note": "一款多功能图像管理工具，其支持超过500种图像格式浏览，并能导出至约70种文件格式。",
+          "icon": "https://www.xnview.com/img/app-xnsoft-512.png"
+        },
+        {
+          "id": "mtlf5ayeo2sy",
+          "name": "PicView",
+          "url": "https://picview.org/",
+          "note": "PicView 是一款免费开源的跨平台图片查看器，该软件基于.NET Avalonia 框架开发，支持 100+ 种图像格式，具有极速加载、无广告、高度可定制等特点 。",
+          "icon": "https://picview.org/favicon.svg"
+        }
+      ]
+    },
+    {
+      "id": "mtjyxdby4r0k",
+      "name": "压缩软件",
+      "icon": "box-archive",
+      "color": "",
+      "links": [
+        {
+          "id": "mtjz0vx7admn",
+          "name": "7-ZIP",
+          "url": "https://www.7-zip.org/",
+          "note": "最经典的压缩软件",
+          "icon": "https://www.7-zip.org/7ziplogo.png"
+        },
+        {
+          "id": "mtjznfnmrdtv",
+          "name": "PeaZip",
+          "url": "https://peazip.cn/",
+          "note": "PeaZip 是一款跨平台的文件压缩与管理工具，支持超过200种压缩格式， 完全免费无广告，为您提供安全、高效的文件处理体验。",
+          "icon": "https://lsw-fast.lenovo.com.cn/appstore/normal/apps/8599-2023-05-10023819-1683700699097.png"
+        },
+        {
+          "id": "mtjzokdu11co",
+          "name": "NanaZip",
+          "url": "https://www.nanazip.org.cn/index.html",
+          "note": "NanaZip 源自 7-Zip 的优秀血脉，融合 Windows 11 的现代设计语言：原生暗色模式、Mica 材质、智能解压与更丰富的上下文菜单。开源免费，安全无广告。",
+          "icon": "https://www.nanazip.org.cn/assets/img/favicon.ico"
+        },
+        {
+          "id": "mtk2w054qpxm",
+          "name": "360压缩国际版",
+          "url": "https://www.360totalsecurity.com/zh-cn/360zip/",
+          "note": "360压缩是360推出的免费解压缩软件,360压缩快速轻巧、兼容性好、永久免费。",
+          "icon": "https://s5.ssl.qhres2.com/static/ac191608d6304d0b.ico"
+        },
+        {
+          "id": "mtk34ubn7xst",
+          "name": "WinRAR",
+          "url": "https://pan.lanzoui.com/b105455",
+          "note": "WinRAR 压缩文件管理器是一款装机必备的压缩软件与文件解压工具.",
+          "icon": "https://www.winrar.com.cn/favicon.ico"
+        }
+      ]
+    },
+    {
+      "id": "mtk0r23kmzn8",
+      "name": "视频播放器",
+      "icon": "video",
+      "color": "",
+      "links": [
+        {
+          "id": "mtk0sgddxc50",
+          "name": "完美解码",
+          "url": "https://jm.wmzhe.com/",
+          "note": "完美解码是一款为众多影视发烧友精心打造的专业高清播放器。超强HDTV支持，画质远超主流播放器！",
+          "icon": "https://jm.wmzhe.com/favicon.ico"
+        },
+        {
+          "id": "mtk0ygiyhiph",
+          "name": "弹弹play",
+          "url": "https://www.dandanplay.com/",
+          "note": "弹弹play 是一款免费的全功能播放器：播放硬盘里的高清视频，同时观看和发送在线弹幕。智能识别、追番同步、AI 字幕、BT 下载，一应俱全。",
+          "icon": "https://www.dandanplay.com/favicon.ico"
+        },
+        {
+          "id": "mtoclbdyhu97",
+          "name": "KRed播放器",
+          "url": "https://kingjetapps.com/kredplayer.html",
+          "note": "KRed播放器是一款Windows本地视频播放器，支持MP4、MKV、AVI、MOV等主流音视频格式，硬解4K，流畅省资源。",
+          "icon": "https://kingjetapps.com/img/kredplayer.png"
+        },
+        {
+          "id": "mtod0hla025a",
+          "name": "NipaPlay",
+          "url": "https://nipaplay.aimes-soft.com/",
+          "note": "现代、美观、强大的视频播放器。专为极致影音体验而生，无论是本地收藏还是线上追剧，NipaPlay 都能为您带来前所未有的流畅与便捷。",
+          "icon": "https://nipaplay.aimes-soft.com/assets/icon.webp"
+        }
+      ]
+    },
+    {
+      "id": "mtk5cgmc2tm0",
+      "name": "杀毒安全",
+      "icon": "shield",
+      "color": "",
+      "links": [
+        {
+          "id": "mtk5dr4hdxed",
+          "name": "悟空安全",
+          "url": "https://wukongsafe.cn/",
+          "note": "用极简架构与前沿技术，给用户新一代安全软件。",
+          "icon": "https://s0.ssl.qhres2.com/static/734af25879acb1fa.ico"
+        },
+        {
+          "id": "mtk5hh49odr0",
+          "name": "火绒安全",
+          "url": "https://www.huorong.cn/",
+          "note": "火绒安全是一款面向终端用户的安全管理平台，主要提供病毒查杀、防护中心、访问控制、安全工具四大功能",
+          "icon": "https://www.huorong.cn/favicon.png"
+        },
+        {
+          "id": "mtk5l14ve6p3",
+          "name": "腾讯电脑管家",
+          "url": "https://guanjia.qq.com/main.html",
+          "note": "腾讯电脑管家是由深圳市腾讯计算机系统有限公司推出的一款免费安全软件，拥有安全云库、系统加速、一键清理、实时防护、网速保护、电脑诊所等功能。",
+          "icon": "https://guanjia.qq.com/assets/images/v15/favicon.ico?_t=20230209"
+        }
+      ]
+    },
+    {
+      "id": "mtlfcq77sm09",
+      "name": "运行库",
+      "icon": "screwdriver-wrench",
+      "color": "",
+      "links": [
+        {
+          "id": "mtlffbrv7xwj",
+          "name": "VC++ 运行库集合包",
+          "url": "https://mpvci.co.uk/",
+          "note": "MultiPack Visual C++ 安装程序“MPVCI”——一个简单的 GUI 打包安装程序，适用于所有最新的 MS Visual C++ 重新分发版本。",
+          "icon": "https://www.dayanzai.me/wp-content/uploads/2023/01/MultiPack-Visual-C.png"
+        },
+        {
+          "id": "mtlflzh5ct3y",
+          "name": "微软常用运行库合集",
+          "url": "https://www.nljs.site/ms-runtime-libraries-package.html",
+          "note": "微软常用运行库合集是第三方整理的安装包，集成了系统运行所需的多种基础组件，用于解决软件因缺少 DLL 文件无法启动的问题 。‌‌",
+          "icon": "https://www.dayanzai.me/wp-content/uploads/2017/09/Microsoft-Apps.png"
+        }
+      ]
+    },
+    {
+      "id": "mto9uwhz4l4t",
+      "name": "游戏网站",
+      "icon": "gamepad",
+      "color": "",
+      "links": [
+        {
+          "id": "mto9xkug4ct0",
+          "name": "Steam",
+          "url": "https://store.steampowered.com/",
+          "note": "Steam是由美国Valve公司聘请BitTorrent发明者布拉姆·科恩（Bram Cohen）设计开发，于2003年推出的数字游戏发行与社交平台，现为全球最大的综合性数字发行平台之一。",
+          "icon": "https://store.steamchina.com/favicon.ico"
+        },
+        {
+          "id": "mtoa15vja78n",
+          "name": "Epic Games",
+          "url": "https://store.epicgames.com/",
+          "note": "Epic Games是一家美国电子游戏与软件开发公司，公司核心业务包括游戏开发与虚幻引擎的研发与授权，其旗下拥有风靡全球的游戏《堡垒之夜》以及广泛应用于游戏及其他行业的虚幻引擎。",
+          "icon": "https://static-assets-prod.epicgames.com/epic-store/static/favicon.ico"
+        }
+      ]
+    }
+  ],
+  "settings": {
+    "title": "常用软件下载导航页",
+    "favicon": "img/favicon.ico",
+    "bgDesktop": "",
+    "bgMobile": "",
+    "opacity": 0.4,
+    "theme": "light",
+    "useWallpaper": false
+  },
+  "exportedAt": "2026-09-05T12:35:11.590Z"
+};
